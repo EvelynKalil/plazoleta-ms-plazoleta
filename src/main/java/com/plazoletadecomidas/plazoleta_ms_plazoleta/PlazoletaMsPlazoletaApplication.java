795d97cace07f6a74a0334eeb7e4b9e8752557b7
EvelynKalil <evelyn.rendon@pragma.com.co>
2025-08-05 11:51:09 -0500

    first commit

---
+++ src/main/java/com/plazoletadecomidas/plazoleta_ms_plazoleta/PlazoletaMsPlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.plazoletadecomidas.plazoleta_ms_plazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletaMsPlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletaMsPlazoletaApplication.class, args);
+	}
+
+}
